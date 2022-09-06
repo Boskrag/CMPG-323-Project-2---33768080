5e74c6cd3d53439a90fcfb044fae01a16384f148
Adriaan Louw <alouw133@gmail.com>
2022-09-06 23:36:29 +0200

    Error Correction

--- VisualStudio_API_Project2/Startup.cs
+++ VisualStudio_API_Project2/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VisualStudio_API_Project2.Models;
 
 namespace VisualStudio_API_Project2
 {
@@ -26,6 +28,7 @@ namespace VisualStudio_API_Project2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ConnectedOfficeContext>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
             services.AddSwaggerGen(options => { options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Swagger API ", Version = "v2", Description = "My API using Swagger", }); });
         }
 
